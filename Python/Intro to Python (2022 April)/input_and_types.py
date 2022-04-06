8724715822aaf68660bad93d917ffb5657ee5133
reneenoble <renee.noble@hotmail.com>
2022-04-06 15:37:16 +1000

    Episode 1 - input, output, varibales, types

---
+++ Python/Intro to Python (2022 April)/input_and_types.py
@@ -0,0 +1,9 @@
+# name = input("What is your name? ")
+# print("hello", name)
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+print(num1 + num2)
+
+# print(type(name))
+print(type(num1))
